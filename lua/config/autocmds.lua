2ec38623ef13af9edb8ca9d867bfa0d06ae88442
Brian Procopio <brian.procopio@somersetcapital.com>
2024-03-25 12:27:47 -0400

    Added session manager plugin

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,26 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+local config_group = vim.api.nvim_create_augroup("MyConfigGroup", {}) -- A global group for all your config autocommands
+
+vim.api.nvim_create_autocmd({ "User" }, {
+  pattern = "SessionLoadPost",
+  group = config_group,
+  callback = function()
+    require("neo-tree.api").tree.toggle(false, true)
+  end,
+})
+
+-- Auto save session
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+  callback = function()
+    local session_manager = require("session_manager")
+    for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+      -- Don't save while there's any 'nofile' buffer open.
+      if vim.api.nvim_get_option_value("buftype", { buf = buf }) == "nofile" then
+        return
+      end
+    end
+    session_manager.save_current_session()
+  end,
+})
--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,6 +1,8 @@
 return {
   "folke/which-key.nvim",
   opts = function(_, opts)
+    local LazyVim = require("lazyvim.util")
+
     if LazyVim.has("ChatGPT.nvim") then
       opts.defaults["<leader>p"] = {
         name = "ChatGPT [p]rompt",
@@ -17,6 +19,18 @@ return {
         x = { "<cmd>ChatGPTRun explain_code<CR>", "Explain Code", mode = { "n", "v" } },
         r = { "<cmd>ChatGPTRun roxygen_edit<CR>", "Roxygen Edit", mode = { "n", "v" } },
         l = { "<cmd>ChatGPTRun code_readability_analysis<CR>", "Code Readability Analysis", mode = { "n", "v" } },
+      }
+    end
+
+    if LazyVim.has("neovim-session-manager") then
+      opts.defaults["<leader>m"] = {
+        name = "Session [m]anager",
+        l = { "<cmd>SessionManager load_session<CR>", "Load Session...", mode = { "n", "v" } },
+        m = { "<cmd>SessionManager load_current_dir_session<CR>", "Load Last Session for CWD", mode = { "n", "v" } },
+        L = { "<cmd>SessionManager load_last_session<CR>", "Load Last Saved Session", mode = { "n", "v" } },
+        s = { "<cmd>SessionManager save_current_session<CR>", "Save CWD Session", mode = { "n", "v" } },
+        d = { "<cmd>SessionManager delete_session<CR>", "Delete Session...", mode = { "n", "v" } },
+        D = { "<cmd>SessionManager delete_current_dir_session<CR>", "Delete CWD Session", mode = { "n", "v" } },
       }
     end
   end,
