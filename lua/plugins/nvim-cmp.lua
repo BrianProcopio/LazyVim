23315da1f6dc74e359afca01636b0f7d4ae545f0
Brian Procopio <bprocopio@sugarplumsoftware.com>
2024-03-30 19:53:37 -0400

    x no longer copies to clipboard. First item in completion no longer selected automatically

---
+++ lua/plugins/nvim-cmp.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "hrsh7th/nvim-cmp",
+    opts = function(_, opts)
+      local cmp = require("cmp")
+
+      opts.preselect = cmp.PreselectMode.None
+      opts.completion = {
+        completeopt = "menu,menuone,noinsert,noselect",
+      }
+
+      opts.mapping = cmp.mapping.preset.insert(vim.tbl_deep_extend("force", opts.mapping, {
+        -- My config
+        ["<CR>"] = cmp.mapping.confirm({ select = false }), -- 'select = false' to only confirm explicitly selected item
+      }))
+    end,
+  },
+}
