14d6177684739c08cee057bbc0689f384029e173
Brian Procopio <bprocopio@sugarplumsoftware.com>
2026-03-13 20:11:24 -0400

    Got rid of the trash markdown linter

--- lua/plugins/linters.lua
+++ lua/plugins/linters.lua
@@ -2,11 +2,36 @@ return {
   {
     "mfussenegger/nvim-lint",
     opts = {
-      linters = {
-        markdownlint = {
-          args = { "--disable", "MD013", "--" },
-        },
+      linters_by_ft = {
+        markdown = {},
       },
     },
+  },
+  {
+    "nvimtools/none-ls.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.sources = vim.tbl_filter(function(source)
+        return source.name ~= "markdownlint-cli2"
+      end, opts.sources or {})
+    end,
+  },
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = {
+      formatters_by_ft = {
+        markdown = { "prettier" },
+        ["markdown.mdx"] = { "prettier" },
+      },
+    },
+  },
+  {
+    "mason-org/mason.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed = vim.tbl_filter(function(tool)
+        return tool ~= "markdownlint-cli2"
+      end, opts.ensure_installed or {})
+    end,
   },
 }
